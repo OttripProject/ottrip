14f7e338f2998544876d3177ebfd8291e6796e34
sehooh5 <sehooh5@gmail.com>
2026-01-21 12:30:01 +0900

    Revert "webview test"
    This reverts commit 5b726cbac9b3e3d9ebe56c251528008ed6c83075.

--- client/src/screens/AuthCallbackScreen.tsx
+++ client/src/screens/AuthCallbackScreen.tsx
@@ -27,20 +27,12 @@ export default function AuthCallbackScreen() {
     const params = new URLSearchParams(hash.startsWith('#') ? hash.substring(1) : hash);
     const idToken = params.get('id_token');
     
-    const isPopup = window.opener && !window.opener.closed;
-    
     const run = async () => {
       if (!idToken) {
-        if (isPopup) {
-          window.opener?.postMessage({ type: 'google_oauth_error', error: 'No id_token' }, window.location.origin);
-          window.close();
-        } else {
-          // @ts-ignore
-          navigation.replace('로그인');
-        }
+        // @ts-ignore
+        navigation.replace('로그인');
         return;
       }
-      
       try {
         const response = await authApi.googleLogin(idToken);
         let email: string | undefined = undefined;
@@ -50,50 +42,36 @@ export default function AuthCallbackScreen() {
           const obj = JSON.parse(json);
           email = typeof obj?.email === 'string' ? obj.email : undefined;
         } catch {}
-        
-        if (isPopup) {
-          window.opener?.postMessage(
-            { type: 'google_oauth_token', id_token: idToken },
-            window.location.origin
-          );
-          window.close();
-        } else {
-          if (response.isRegistered) {
-            await login({
-              isRegistered: true,
-              accessToken: response.accessToken,
-              refreshToken: response.refreshToken,
-            });
-            
-            try {
-              const token = Platform.OS === 'web'
-                ? window.localStorage.getItem('pendingInviteToken')
-                : await SecureStore.getItemAsync('pendingInviteToken');
-              if (token) {
-                await api.post(`/private/plans/invitations/${token}/accept`);
-                if (Platform.OS === 'web') {
-                  window.localStorage.removeItem('pendingInviteToken');
-                  window.dispatchEvent(new Event('plans-refresh'));
-                } else {
-                  await SecureStore.deleteItemAsync('pendingInviteToken');
-                }
+        if (response.isRegistered) {
+          await login({
+            isRegistered: true,
+            accessToken: response.accessToken,
+            refreshToken: response.refreshToken,
+          });
+          
+          try {
+            const token = Platform.OS === 'web'
+              ? window.localStorage.getItem('pendingInviteToken')
+              : await SecureStore.getItemAsync('pendingInviteToken');
+            if (token) {
+              await api.post(`/private/plans/invitations/${token}/accept`);
+              if (Platform.OS === 'web') {
+                window.localStorage.removeItem('pendingInviteToken');
+                window.dispatchEvent(new Event('plans-refresh'));
+              } else {
+                await SecureStore.deleteItemAsync('pendingInviteToken');
               }
-            } catch {}
-          } else {
-            await login(response);
-            // @ts-ignore
-            navigation.replace('약관동의', { registerToken: response.registerToken, prefill: response.prefill, email });
-            return;
-          }
+            }
+          } catch {}
+        } else {
+          await login(response);
+          // @ts-ignore
+          navigation.replace('약관동의', { registerToken: response.registerToken, prefill: response.prefill, email });
+          return;
         }
       } catch (error: any) {
-        if (isPopup) {
-          window.opener?.postMessage({ type: 'google_oauth_error', error: 'Login failed' }, window.location.origin);
-          window.close();
-        } else {
-          // @ts-ignore
-          navigation.replace('로그인');
-        }
+        // @ts-ignore
+        navigation.replace('로그인');
       }
     };
     run();
